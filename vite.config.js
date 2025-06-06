1196f0dc35d04318c917750104c2edcd041a9264
Zane1732 <112121784+Zane1732@users.noreply.github.com>
2025-06-06 23:42:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {allowedHosts: ['api.varunaditya.xyz']},
+  server: {allowedHosts: ['api.z-movies-powerd-by-zaneflix-m9wx.vercel.app']},
   plugins: [tailwindcss(),]
 });
